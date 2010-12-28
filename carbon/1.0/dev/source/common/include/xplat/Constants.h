e2c38d1d6edd234be81c50455fa3327f803a33b0
saurabhgupta1403 <saurabhgupta1403@e16f9d33-a4db-0b4f-5d1a-325b24bf4611>
2010-12-28 16:16:49 +0000

    Log Module ( a wrapper over log4cxx) implemented and tested.

--- carbon/1.0/dev/source/common/include/xplat/Constants.h
+++ carbon/1.0/dev/source/common/include/xplat/Constants.h
@@ -1,11 +1,25 @@
 
 #pragma once
 
+#include <string>
 
 #ifdef WIN32
 
+	#include <tchar.h>
 
+	#define OSConst(x)		_T(x)	
+	#define OSSlash		OSConst("\\")
 
+	typedef TCHAR OSChar;
+	typedef std::wstring OSString;
+
+#else
+
+	#define OSConst(x)		x			
+	#define OSSlash		OSConst("/")
+
+	typedef char OSChar;
+	tyepdef std::string OSString
 
 #endif
 
