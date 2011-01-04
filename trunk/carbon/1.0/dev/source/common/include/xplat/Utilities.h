63ca88950ccc8e84812451e93af20540190f8ba2
saurabhgupta1403 <saurabhgupta1403@e16f9d33-a4db-0b4f-5d1a-325b24bf4611>
2011-01-04 18:53:10 +0000

    secure key container has the feature to init from key stream and generate a key stream

--- trunk/carbon/1.0/dev/source/common/include/xplat/Utilities.h
+++ trunk/carbon/1.0/dev/source/common/include/xplat/Utilities.h
@@ -11,6 +11,10 @@ namespace carbonUtilities
 {
 
 	bool cuFileExists(const OSString &filePath);
+	bool getIntFromCharArray(int &outVal, const char *in, unsigned int start_index = 0, unsigned int size_chars = 0);
+
+	//chars_count denotes how many chars should be there in the array. if 0 or smaller than required, it would create of appropriate length.
+	bool getCharArrayFromInt(char **outArr, unsigned int i, unsigned int chars_count = 0);
 
 
 
