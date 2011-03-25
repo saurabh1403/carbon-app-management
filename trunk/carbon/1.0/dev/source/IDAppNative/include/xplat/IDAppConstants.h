fa7a97714df83137e490c8eb204f9a7fb993f52e
saurabhgupta1403 <saurabhgupta1403@e16f9d33-a4db-0b4f-5d1a-325b24bf4611>
2011-03-25 09:18:32 +0000

    some constants file added. (IDAppConstants.h)

---
+++ trunk/carbon/1.0/dev/source/IDAppNative/include/xplat/IDAppConstants.h
@@ -0,0 +1,18 @@
+
+#pragma once
+
+#include "Constants.h"
+
+#define ResultXmlNodeName			"result"
+#define ResultXmlNode				"<result/>"
+
+#define OutputXmlNode				"<outputXml></outputXml>"
+#define CallIdentifierNodeName		"CallIdentifier"
+
+#define XmlInitErrorNode			"<error>Failed to init Xml</error>"
+#define XmlProcessingErrorNode		"<error>Failed to process error</error>"
+
+#define PackageIdNodeXquery			OSConst("//argumentXml/packageId")
+
+
+
